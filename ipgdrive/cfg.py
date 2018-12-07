8db38d8980ee6469faa527886e147120d78a92ea
Shay Palachy <shaypal5@gmail.com>
2018-12-07 17:04:35 +0200

    everything working nice now

--- ipgdrive/cfg.py
+++ ipgdrive/cfg.py
@@ -15,18 +15,19 @@ class CfgKey(object):
     USERNAME = 'username'
     SPREADSHEET_NAME = 'spreadsheet_name'
     FREQ_MINUTES = 'freq_minutes'
+    SPREADSHEET_KEY = 'spreadsheet_key'
 
 
 IPG_DIRPATH = os.path.expanduser('~/.config/ipgdrive/')
 DRIVE_KEY_FNAME = 'google_drive_service_account_key.json'
 DRIVE_KEY_FPATH = os.path.join(IPG_DIRPATH, DRIVE_KEY_FNAME)
 
-GOOGLE_DRIVE_SPREADSHEET_SCOPE_URL = 'https://spreadsheets.google.com/feeds'
+SCOPE = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
 
 
 def get_authenticated_gspread():
     """Returns an authenticated gspread instance."""
-    scope = [GOOGLE_DRIVE_SPREADSHEET_SCOPE_URL]
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        DRIVE_KEY_FPATH, scope)
+        DRIVE_KEY_FPATH, SCOPE)
     return gspread.authorize(credentials)
--- ipgdrive/job.py
+++ ipgdrive/job.py
@@ -15,13 +15,18 @@ def job_func():
     gc = get_authenticated_gspread()
     ip = get('https://api.ipify.org').text
     print('My public IP address is: {}'.format(ip))
-    sh = gc.open(IPG_CFG[CfgKey.SPREADSHEET_NAME])
+    # sh_name = IPG_CFG[CfgKey.SPREADSHEET_NAME]
+    # print('Trying to open spreadsheet {}'.format(sh_name))
+    sh_key = IPG_CFG[CfgKey.SPREADSHEET_KEY]
+    print('Trying to open spreadsheet with key: {}'.format(sh_key))
+    # sh = gc.open(sh_name)
+    sh = gc.open_by_key(sh_key)
     # sh = gc.create(ip)
     # email = IPG_CFG[CfgKey.EMAIL]
     # sh.share(email, perm_type='user', role='writer')
     worksheet = sh.get_worksheet(0)
     worksheet.update_acell('A1', 'Ubuntu public ip')
-    worksheet.update_acell('A2', 2)
+    worksheet.update_acell('A2', ip)
     worksheet.update_acell('B1', 'Last Time Ran')
     worksheet.update_acell('B2', str(datetime.now()))
 
